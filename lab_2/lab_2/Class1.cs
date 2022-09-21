2ace00881954538f5fae161cc818822390ff5c78
Stacy <nat.kkurag.0301@mail.ru>
2022-09-21 22:51:21 +0300

    Lab_2

---
+++ lab_2/lab_2/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace laba_2
+{
+    public partial class Car
+    {
+        private const string FLAG = "FLAG{dnNqy6T%3|z3ZOZ}";
+        private readonly int CarID;
+
+        // class properties
+        public string CarModel { get; set; }
+        public string CarColor { get; set; }
+        public int CarPrice { get; set; }
+        public string CarNumber { get; private set; }
+        public string CarBrand { get; set; }
+        public int CarYear { get; set; }
+        public static int CarCounter { get; private set; }
+
+        // constructors
+
+        static Car()
+        {
+            CarCounter = 0;
+        }
+
+        public Car()
+        {
+            CarPrice = 0;
+            CarModel = "Undefined";
+            CarColor = "Undefined";
+            CarNumber = "Undefined";
+            CarBrand = "Undefined";
+            CarYear = 0;
+            laba_2.Car.CarCounter++;
+            CarID = GetHashCode();
+
+        }
+
+        public Car(string carModel, string carColor, int carPrice, string carNumber, string carBrand, int carYear)
+        {
+            CarModel = carModel;
+            CarColor = carColor;
+            CarPrice = carPrice;
+            CarNumber = carNumber;
+            CarBrand = carBrand;
+            CarYear = carYear;
+            CarID = GetHashCode();
+        }
+
+        private Car(int usID)
+        {
+            CarPrice = 0;
+            CarModel = "Undefined";
+            CarColor = "Undefined";
+            CarNumber = "Undefined";
+            CarBrand = "Undefined";
+            CarYear = 0;
+        }
+
+        // methods
+
+        public void ShowInfo() => Console.WriteLine($"{this.ToString()}; Hashed id number: {CarID}");
+        public static void ChangeBrand(out string brand, string CarBrand) => brand = CarBrand;
+        public int ShowCarYear() => CarYear;
+        public static Car CreateCar(int Id) => new Car(Id);
+
+    }
+}
---
+++ lab_2/lab_2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace laba_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SecondTask();
+            ThirdTask();
+            FourthTask();
+        }
+
+        public static void SecondTask()
+        {
+            var car1 = new Car();
+            var car2 = new Car("Chevrolet", "Black", 2700, "5674OA-7", "Impala", 1967);
+
+            Console.WriteLine(car1.Equals(car2) ? "Идентичны" : "Не идентичны");
+            Console.WriteLine($"Type of car1 - {car1.GetType()}");
+
+            car2.ShowInfo();
+
+            Console.WriteLine("Ref and Out Parametres :");
+            int car_year = car2.CarYear;
+            car2.CarYear = car_year;
+            Console.WriteLine(car2.CarYear);
+
+            Car.ChangeBrand(out string brand, "Lancer");
+            car2.CarBrand = brand;
+            Console.WriteLine(car2.CarBrand);
+        }
+        public static void ThirdTask()
+        {
+            var Cars = new Car[4];
+            Cars[0] = new Car("Mitsubishi", "Metalic", 20000, "390JJ", "Lancer", 2002);
+            Cars[1] = new Car("Alfa", "Black", 4730, "0223", "Romeo", 2018);
+            Cars[2] = new Car("Nissan", "Blue", 4200, "3920", "Almera", 1999);
+            Cars[3] = new Car("Audi", "Red", 15800, "3920", "A6", 2001);
+
+            foreach (var car in Cars)
+                if (car.CarNumber == "0223")
+                    Console.WriteLine(car.CarPrice);
+
+            Console.WriteLine("Cars older 10 years:");
+            foreach (var car in Cars)
+                if (2022 - car.ShowCarYear() > 10)
+                    Console.WriteLine($"{car.CarModel} : {car.CarYear}");
+
+        }
+
+        public static void FourthTask()
+        {
+            var RandomCar = new { Brand = "Renault", Model = "Laguna", Color = "Gray" };
+            Console.WriteLine($"Car: {RandomCar.Brand} {RandomCar.Model} {RandomCar.Color}");
+        }
+      
+    }
+}
